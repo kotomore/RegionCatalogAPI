bb757c29d3087790ca2716a11effd13674908b52
set404 <89992008819@mail.ru>
2023-07-23 13:55:54 +0300

    feat: added tests

---
+++ src/test/java/ru/kotomore/regioncatalogapi/controllers/RegionControllerTest.java
@@ -0,0 +1,157 @@
+package ru.kotomore.regioncatalogapi.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.kotomore.regioncatalogapi.dto.CreateRegionRequest;
+import ru.kotomore.regioncatalogapi.dto.RegionResponse;
+import ru.kotomore.regioncatalogapi.dto.UpdateRegionRequest;
+import ru.kotomore.regioncatalogapi.services.RegionService;
+import ru.kotomore.regioncatalogapi.services.RegionServiceUseCase;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class RegionControllerTest {
+
+    @Mock
+    private RegionServiceUseCase regionService;
+
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @BeforeEach
+    public void setUp() {
+        regionService = Mockito.mock(RegionService.class);
+    }
+
+    @Test
+    public void testGetAllRegions_ShouldReturnAllRegionsList() throws Exception {
+        RegionController regionController = new RegionController(regionService);
+        mockMvc = MockMvcBuilders.standaloneSetup(regionController).build();
+
+        when(regionService.findAll()).thenReturn(List.of(getRegionResponse(1L), getRegionResponse(2L)));
+
+        mockMvc.perform(get("/api/regions"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    public void testGetRegionsByName_ShouldReturnRegionsList() throws Exception {
+        RegionController regionController = new RegionController(regionService);
+        mockMvc = MockMvcBuilders.standaloneSetup(regionController).build();
+        String name = "abc";
+
+        when(regionService.findByName(name)).thenReturn(List.of(getRegionResponse(1L), getRegionResponse(2L)));
+
+        mockMvc.perform(get("/api/regions")
+                        .param("name", name))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    public void testGetRegionsByAbbreviation_ShouldReturnRegionsList() throws Exception {
+        RegionController regionController = new RegionController(regionService);
+        mockMvc = MockMvcBuilders.standaloneSetup(regionController).build();
+        String abbreviation = "abc";
+
+        when(regionService.findByAbbreviation(abbreviation)).thenReturn(List.of(getRegionResponse(1L), getRegionResponse(2L)));
+
+        mockMvc.perform(get("/api/regions")
+                        .param("abbreviation", abbreviation))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    public void testGetRegionsByAbbreviationAndName_ShouldThrowsException() throws Exception {
+        RegionController regionController = new RegionController(regionService);
+        mockMvc = MockMvcBuilders.standaloneSetup(regionController).build();
+
+        mockMvc.perform(get("/api/regions")
+                        .param("abbreviation", "abbreviation")
+                .param("name", "name"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetRegionsById_ShouldReturnOk() throws Exception {
+        RegionController regionController = new RegionController(regionService);
+        mockMvc = MockMvcBuilders.standaloneSetup(regionController).build();
+
+        RegionResponse regionResponse = getRegionResponse(1L);
+
+        when(regionService.findById(1L)).thenReturn(regionResponse);
+
+        mockMvc.perform(get("/api/regions/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(regionResponse.name()));
+    }
+
+    @Test
+    public void testCreateRegion_ShouldReturnOk() throws Exception {
+        RegionController regionController = new RegionController(regionService);
+        mockMvc = MockMvcBuilders.standaloneSetup(regionController).build();
+
+        RegionResponse regionResponse = getRegionResponse(1L);
+        CreateRegionRequest createRegionRequest = new CreateRegionRequest("name", "ABBR");
+
+        when(regionService.save(any())).thenReturn(regionResponse);
+
+        mockMvc.perform(post("/api/regions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createRegionRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(regionResponse.name()));
+    }
+
+    @Test
+    public void testUpdateRegion_ShouldReturnOk() throws Exception {
+        RegionController regionController = new RegionController(regionService);
+        mockMvc = MockMvcBuilders.standaloneSetup(regionController).build();
+
+        RegionResponse regionResponse = getRegionResponse(1L);
+        UpdateRegionRequest updateRegionRequest = new UpdateRegionRequest(1L, "name", "ABBR");
+
+        when(regionService.update(any())).thenReturn(regionResponse);
+
+        mockMvc.perform(put("/api/regions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRegionRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(regionResponse.name()));
+    }
+
+    @Test
+    public void testDeleteRegionById_ShouldReturnOk() throws Exception {
+        RegionController regionController = new RegionController(regionService);
+        mockMvc = MockMvcBuilders.standaloneSetup(regionController).build();
+
+        mockMvc.perform(delete("/api/regions/1"))
+                .andExpect(status().isOk());
+    }
+
+    private static RegionResponse getRegionResponse(Long id) {
+        return new RegionResponse(id, "some name", "some abbreviation");
+    }
+}
+
---
+++ src/test/java/ru/kotomore/regioncatalogapi/services/RegionServiceTest.java
@@ -0,0 +1,177 @@
+package ru.kotomore.regioncatalogapi.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.kotomore.regioncatalogapi.dto.RegionResponse;
+import ru.kotomore.regioncatalogapi.dto.UpdateRegionRequest;
+import ru.kotomore.regioncatalogapi.entities.Region;
+import ru.kotomore.regioncatalogapi.exceptions.RegionNotDeletedException;
+import ru.kotomore.regioncatalogapi.exceptions.RegionNotFoundException;
+import ru.kotomore.regioncatalogapi.exceptions.RegionNotSavedException;
+import ru.kotomore.regioncatalogapi.repositories.RegionRepository;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class RegionServiceTest {
+
+    @Mock
+    RegionRepository regionRepository;
+
+    @InjectMocks
+    RegionService regionService;
+
+
+    @Test
+    public void testFindById_ExistingId_ReturnsRegionResponse() {
+
+        Long id = 1L;
+        Region region = getRegion(id);
+
+        when(regionRepository.findById(id)).thenReturn(Optional.of(region));
+
+        RegionResponse result = regionService.findById(id);
+        assertNotNull(result);
+        assertEquals(region.getId(), result.id());
+    }
+
+    @Test
+    public void testFindById_NonExistingId_ThrowsRegionNotFoundException() {
+
+        Long nonExistingId = 100L;
+        when(regionRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        assertThrows(RegionNotFoundException.class, () -> regionService.findById(nonExistingId));
+    }
+
+    @Test
+    public void testFindAll_ReturnsListOfRegionResponse() {
+
+        List<Region> regions = Arrays.asList(getRegion(1L), getRegion(2L));
+        when(regionRepository.findAll()).thenReturn(regions);
+
+        List<RegionResponse> result = regionService.findAll();
+        assertNotNull(result);
+        assertEquals(regions.size(), result.size());
+    }
+
+    @Test
+    public void testUpdate_ValidRegionRequest_ReturnsUpdatedRegionResponse() {
+        Long id = 1L;
+        UpdateRegionRequest regionRequest = getUpdateRegionRequest(id);
+        Region regionToUpdate = getRegion(id);
+        when(regionRepository.update(regionToUpdate)).thenReturn(true);
+
+        RegionResponse result = regionService.update(regionRequest);
+
+        assertNotNull(result);
+        assertEquals(regionRequest.id(), result.id());
+        assertEquals(regionRequest.name(), result.name());
+        assertEquals(regionRequest.abbreviation(), result.abbreviation());
+
+    }
+
+    @Test
+    public void testUpdate_InvalidRegionRequest_ThrowsRegionNotSavedException() {
+        UpdateRegionRequest invalidRegionRequest = getUpdateRegionRequest(1L);
+        when(regionRepository.update(any(Region.class))).thenReturn(false);
+
+        assertThrows(RegionNotSavedException.class, () -> regionService.update(invalidRegionRequest));
+    }
+
+    @Test
+    public void testDeleteById_ExistingId_DeletesRegion() {
+        Long idToDelete = 1L;
+        when(regionRepository.deleteById(idToDelete)).thenReturn(true);
+
+        assertDoesNotThrow(() -> regionService.deleteById(idToDelete));
+    }
+
+    @Test
+    public void testDeleteById_NonExistingId_ThrowsRegionNotDeletedException() {
+        Long nonExistingId = 100L;
+        when(regionRepository.deleteById(nonExistingId)).thenReturn(false);
+
+        assertThrows(RegionNotDeletedException.class, () -> regionService.deleteById(nonExistingId));
+    }
+
+    @Test
+    public void testDeleteAll_DeletesAllRegions() {
+        when(regionRepository.deleteAll()).thenReturn(true);
+
+        assertDoesNotThrow(regionService::deleteAll);
+    }
+
+    @Test
+    public void testDeleteAll_FailedToDelete_ThrowsRegionNotDeletedException() {
+        when(regionRepository.deleteAll()).thenReturn(false);
+
+        assertThrows(RegionNotDeletedException.class, regionService::deleteAll);
+    }
+
+    @Test
+    public void testFindByAbbreviation_ExistingAbbreviation_ReturnsListOfRegionResponse() {
+        String abbreviation = "AB";
+        List<Region> regions = Arrays.asList(getRegion(1L), getRegion(2L));
+        when(regionRepository.findByAbbreviation(abbreviation)).thenReturn(regions);
+
+        List<RegionResponse> result = regionService.findByAbbreviation(abbreviation);
+
+        assertNotNull(result);
+        assertEquals(regions.size(), result.size());
+    }
+
+    @Test
+    public void testFindByAbbreviation_NonExistingAbbreviation_ReturnsEmptyList() {
+        String nonExistingAbbreviation = "XYZ";
+        when(regionRepository.findByAbbreviation(nonExistingAbbreviation)).thenReturn(Collections.emptyList());
+
+        List<RegionResponse> result = regionService.findByAbbreviation(nonExistingAbbreviation);
+
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testFindByName_ExistingName_ReturnsListOfRegionResponse() {
+        String name = "Region Name";
+        List<Region> regions = Arrays.asList(getRegion(1L), getRegion(2L));
+        when(regionRepository.findByName(name)).thenReturn(regions);
+
+        List<RegionResponse> result = regionService.findByName(name);
+
+        assertNotNull(result);
+        assertEquals(regions.size(), result.size());
+    }
+
+    @Test
+    public void testFindByName_NonExistingName_ReturnsEmptyList() {
+        // Arrange
+        String nonExistingName = "Non-Existent Region";
+        when(regionRepository.findByName(nonExistingName)).thenReturn(Collections.emptyList());
+
+        List<RegionResponse> result = regionService.findByName(nonExistingName);
+
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+
+    private static Region getRegion(Long id) {
+        return new Region(id, "Some name", "Some abbreviation");
+    }
+
+    private static UpdateRegionRequest getUpdateRegionRequest(Long id) {
+        return new UpdateRegionRequest(id, "Some name", "Some abbreviation");
+    }
+}
